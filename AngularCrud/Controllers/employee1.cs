8939bd9fbfd8987da8a5396f512012ef33c79d84
Workpulse <Workpulse@CHETAN>
2021-08-10 18:59:03 +0530

    Add project files.

---
+++ AngularCrud/Controllers/employee1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularCrud.Controllers
+{
+    public class employee1
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public int Age { get; set; } 
+    }
+}
---
+++ AngularCrud/Scripts/Angularcode.js
@@ -0,0 +1,81 @@
+﻿var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope, $http) {
+    debugger;
+    $scope.InsertData = function () {
+        var Action = document.getElementById("btnSave").getAttribute("value");
+        if (Action == "Submit") {
+            $scope.Employee = {};
+            $scope.Employee.FirstName = $scope.FirstName;
+            $scope.Employee.LastName = $scope.LastName;
+
+            $scope.Employee.City = $scope.City;
+            $scope.Employee.Age = $scope.Age;
+            $http({
+                method: "post",
+                url: "https://localhost:44375/Employee/Insert_Employee",
+                datatype: "json",
+                data: JSON.stringify($scope.Employe)
+            }).then(function (response) {
+                alert(response.data);
+                $scope.GetAllData();
+                $scope.EmpFirstName = "";
+                $scope.EmpLastName = "";
+                $scope.EmpCity = "";
+                $scope.EmpAge = "";
+            })
+        } else {
+            $scope.Employe = {};
+            $scope.Employe.EmpFirstName = $scope.EmpName;
+            $scope.Employe.EmpLast = $scope.EmpCity;
+            $scope.Employe.EmpAge = $scope.EmpAge;
+            $scope.Employe.EmpId = document.getElementById("EmpID_").value;
+            $http({
+                method: "post",
+                url: "https://localhost:44375/Employee/Update_Employee",
+                datatype: "json",
+                data: JSON.stringify($scope.Employe)
+            }).then(function (response) {
+                alert(response.data);
+                $scope.GetAllData();
+                $scope.EmpFirstName = "";
+                $scope.EmpLastName = "";
+                $scope.EmpCity = "";
+                $scope.EmpAge = "";
+                document.getElementById("btnSave").setAttribute("value", "Submit");
+                document.getElementById("btnSave").style.backgroundColor = "cornflowerblue";
+                document.getElementById("spn").innerHTML = "Add New Employee";
+            })
+        }
+    }
+    $scope.GetAllData = function () {
+        $http({
+            method: "get",
+            url: "https://localhost:44375/Employee/Get_AllEmployee"
+        }).then(function (response) {
+            $scope.employees = response.data;
+        }, function () {
+            alert("Error Occur");
+        })
+    };
+    $scope.DeleteEmp = function (Emp) {
+        $http({
+            method: "post",
+            url: "https://localhost:44375/Employee/Delete_Employee",
+            datatype: "json",
+            data: JSON.stringify(Emp)
+        }).then(function (response) {
+            alert(response.data);
+            $scope.GetAllData();
+        })
+    };
+    $scope.UpdateEmp = function (Emp) {
+        document.getElementById("ID").value = Emp.Id;
+        $scope.EmpFirstName = Emp.FirstName;
+        $scope.EmpLastName = Emp.LastName;
+        $scope.EmpCity = Emp.City;
+        $scope.EmpAge = Emp.Age;
+        document.getElementById("btnSave").setAttribute("value", "Update");
+        document.getElementById("btnSave").style.backgroundColor = "Blue";
+        document.getElementById("spn").innerHTML = "Update Employee Information";
+    }
+})
